7b786bdcb79e6797b93aeb07935de9308c50e1dd
Lanxin Lu <lluu0032@student.monash.edu>
2025-08-28 18:02:53 +1000

    Add Home Page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Feeling Care</router-link>
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/support">Get Support</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/resources">Information & Resources</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/involved">Get Involved</router-link>
+          </li>
+          <li class="nav-item"><router-link class="nav-link" to="/about">About Us</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/search">Search</router-link></li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/auth">Login/Signup</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-primary">Feeling Care is Here to Help</h1>
+    <p>Your trusted mental health companion for youth.</p>
+  </div>
+</template>
