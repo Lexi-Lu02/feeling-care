85c3892a925b85b4393e26144fa01c7b68b0c481
Lanxin Lu <lluu0032@student.monash.edu>
2025-10-19 03:40:30 +1100

    Debug.

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    cloudflare({
-      functions: {
-        directory: './src/functions',
-      },
-    }),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
